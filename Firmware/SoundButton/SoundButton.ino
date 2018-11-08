6c3d1f836d553e7427d0c740f7afad54e6861d84
jerome-labidurie <jerome@labidurie.fr>
2018-11-09 00:20:07 +0100

    fw with 1 interrupt

---
+++ Firmware/SoundButton/SoundButton.ino
@@ -0,0 +1,118 @@
+// this example will play a random track from all on the sd 
+//
+// it expects the sd card to contain some mp3 files
+
+#include <SoftwareSerial.h>
+#include <DFMiniMp3.h>
+
+// implement a notification class,
+// its member methods will get called 
+//
+class Mp3Notify
+{
+public:
+  static void OnError(uint16_t errorCode)
+  {
+    // see DfMp3_Error for code meaning
+    Serial.println();
+    Serial.print("Com Error ");
+    Serial.println(errorCode);
+  }
+  static void OnPlayFinished(uint16_t track)
+  {
+    Serial.print("Play finished for #");
+    Serial.println(track);  
+  }
+  static void OnCardOnline(uint16_t code)
+  {
+    Serial.println("Card online ");
+  }
+  static void OnCardInserted(uint16_t code)
+  {
+    Serial.println("Card inserted ");
+  }
+  static void OnCardRemoved(uint16_t code)
+  {
+    Serial.println("Card removed ");
+  }
+};
+
+// instance a DFMiniMp3 object, 
+// defined with the above notification class and the hardware serial class
+//
+//DFMiniMp3<HardwareSerial, Mp3Notify> mp3(Serial1);
+
+// Some arduino boards only have one hardware serial port, so a software serial port is needed instead.
+// comment out the above definition and uncomment these lines
+SoftwareSerial secondarySerial(10, 11); // RX, TX
+DFMiniMp3<SoftwareSerial, Mp3Notify> mp3(secondarySerial);
+
+uint16_t count = 0;
+volatile uint16_t current = 0;
+volatile bool play = false;
+
+void setup() 
+{
+  
+  Serial.begin(115200);
+
+  Serial.println("initializing...");
+
+  pinMode(2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), click, FALLING);
+  
+  mp3.begin();
+  mp3.reset(); 
+  
+  // show some properties and set the volume
+  uint16_t volume = mp3.getVolume();
+  Serial.print("volume ");
+  Serial.println(volume);
+  mp3.setVolume(16);
+  
+  count = mp3.getTotalTrackCount();
+  Serial.print("files ");
+  Serial.println(count);
+
+  uint16_t mode = mp3.getPlaybackMode();
+  Serial.print("playback mode ");
+  Serial.println(mode);
+  
+  Serial.println("starting...");
+
+//  mp3.playRandomTrackFromAll(); // random of all folders on sd
+}
+
+void click() {
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  // If interrupts come faster than 200ms, assume it's a bounce and ignore
+  if (interrupt_time - last_interrupt_time > 200) {
+    current = ( (current + 1) % count );
+    play = true;
+  }
+  last_interrupt_time = interrupt_time;
+}
+void waitMilliseconds(uint16_t msWait)
+{
+  uint32_t start = millis();
+  
+  while ((millis() - start) < msWait)
+  {
+    // calling mp3.loop() periodically allows for notifications 
+    // to be handled without interrupts
+    mp3.loop(); 
+    delay(1);
+  }
+}
+void loop() 
+{
+  if (play == true) {
+    play = false;
+    mp3.playGlobalTrack (current+1);
+  }
+  waitMilliseconds(50);
+  Serial.print(play);
+  Serial.println (current);
+}
+
